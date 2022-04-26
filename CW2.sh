3bc33bb13a7eba2bc08bbfec30ce08b9ccb26a5e
Lim Han Jet <alexlim193@gmail.com>
2022-04-26 18:49:46 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -6,4 +6,4 @@ echo "HTML: $html"
 a=$(echo "$html" | grep '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>' -A1 | cut -d ">" -f 1 | sed -n 2p | xargs)
 echo "$a"
 b=$(echo "$html" | grep '<class="col-span-1 text-xs text-gray-500 text-right tracking-tighter leading-3" data-v-1e2a93af>' -A3 | sed 's/<[^>]*>//g;s/^.//' | xargs)
-echo "$b" | cut -d "+" -f 2 
+echo "$b" | cut -d "+" -f 1 

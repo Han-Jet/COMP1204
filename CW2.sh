6c34bac972ced04a540abfa62f721d14da7ceb1d
Lim Han Jet <alexlim193@gmail.com>
2022-04-26 21:56:36 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -5,5 +5,5 @@ html=$(curl --silent https://covidnow.moh.gov.my/)
 echo "HTML: $html"
 a=$(echo "$html" | grep '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>' -A1 | cut -d ">" -f 1 | sed -n 2p | xargs)
 echo "$a" | sed 's/[+,]//g'
-b=$(echo "$html" | grep '<span data-v-1e2a93af data-v-3ab42af2>')
+b=$(echo "$html" | grep '<span data-v-1e2a93af data-v-3ab42af2>' +2,478)
 echo "$b"

9f4809e6245b2fadc3b80fd5db8452d1294419f2
Lim <hjl1c21>
2022-04-26 10:48:02 +0800

    Add CW2.sh

---
+++ CW2.sh
@@ -0,0 +1,5 @@
+#curl https://covidnow.moh.gov.my/ > data_v1.html
+html=$(curl --silent https://covidnow.moh.gov.my/)
+echo "HTML: $html"
+a=$($html | grep '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>' -A1 | cut -d ">" -f 1 | sed -n 2p | xargs
+echo "$a"

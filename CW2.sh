4ede3e972fc5874268d860a5a52a36bdf9491f01
Lim Han Jet <alexlim193@gmail.com>
2022-04-27 09:37:02 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -10,4 +10,4 @@ echo "$b" | sed 's/[+]//g'
 c=$(echo "$html" | grep '<span data-v-1e2a93af data-v-3ab42af2>Daily - Tests</span>' -A1 | cut -d ">" -f 1 | sed -n 2p | xargs)
 echo "$c" | sed 's/[+]//g'
 d=$(echo "$html" | grep '<span data-v-1e2a93af data-v-3ab42af2>Positivity Rate</span>' -A1 | cut -d ">" -f 1 | sed -n 2p | xargs)
-echo "$d" | sed 's/[+]//g'
+echo "$d"

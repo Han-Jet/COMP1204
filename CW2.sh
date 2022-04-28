7808502a52c8aafc0aacf9b42c7fbe562099e01e
Lim Han Jet <alexlim193@gmail.com>
2022-04-28 10:28:01 +0800

    Fixed a bug which displays the same data twice

--- CW2.sh
+++ CW2.sh
@@ -14,16 +14,12 @@ clean_code() {
 }
 
 clean_code '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>'
-#a=$(clean_code '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>')
-#echo "$a"
+
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Cases</span></div>'
-b=$(clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Cases</span></div>')
-echo "$b"
+
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Tests</span>'
-c=$(clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Tests</span>')
-echo "$c"
+
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Positivity Rate</span>'
-d=$(clean_code '<span data-v-1e2a93af data-v-3ab42af2>Positivity Rate</span>')
-echo "$d"
+
 e=$(echo "$html" | grep "Deaths due to COVID" -A2 | tail -n 1 | xargs)
 echo "$e" | sed 's/[+]//g'

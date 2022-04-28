f40b6dfccbbefadc0245b7310fb2880633215495
Lim Han Jet <alexlim193@gmail.com>
2022-04-28 14:13:29 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -4,11 +4,12 @@ html=$(curl --silent https://covidnow.moh.gov.my/)
 echo "HTML: $html"
 
 clean_code() {
-	echo "$html" | grep "$1" -A1 | cut -d ">" -f 1 | sed -n 2p | xargs | sed 's/[+]//g'
+	tempCode=$(echo "$html" | grep "$1" -A1 | cut -d ">" -f 1 | sed -n 2p | xargs | sed 's/[+]//g')
 }
 
 clean_code '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>'
-
+a=$tempCode
+echo "$a"
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Cases</span>'
 
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Tests</span>'

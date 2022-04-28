198cdba3896c8048e22264355e6d2bf8c7946b90
Lim Han Jet <alexlim193@gmail.com>
2022-04-28 10:43:40 +0800

    Added a new data

--- CW2.sh
+++ CW2.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#curl https://covidnow.moh.gov.my/ > data_v1.html
 html=$(curl --silent https://covidnow.moh.gov.my/)
 echo "HTML: $html"
 
 clean_number() {
-	tempN=$(echo "$1" | sed 's/[+,]//g')
-	#return "$tempN"
+	tempN=$(echo "$1" | sed 's/[+]//g')
 }
 
 clean_code() {
@@ -15,7 +13,7 @@ clean_code() {
 
 clean_code '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>'
 
-clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Cases</span></div>'
+clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Cases</span>'
 
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Tests</span>'
 
@@ -23,3 +21,5 @@ clean_code '<span data-v-1e2a93af data-v-3ab42af2>Positivity Rate</span>'
 
 e=$(echo "$html" | grep "Deaths due to COVID" -A2 | tail -n 1 | xargs)
 echo "$e" | sed 's/[+]//g'
+
+clean_code '<span data-v-1e2a93af data-v-3ab42af2>Active - ICU</span>'

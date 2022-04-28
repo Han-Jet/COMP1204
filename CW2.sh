61181a02c4fc77f74b25bdea1fd3a011714174f5
Lim Han Jet <alexlim193@gmail.com>
2022-04-28 14:19:16 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -4,21 +4,27 @@ html=$(curl --silent https://covidnow.moh.gov.my/)
 echo "HTML: $html"
 
 clean_code() {
-	tempCode=$(echo "$html" | grep "$1" -A1 | cut -d ">" -f 1 | sed -n 2p | xargs | sed 's/[+]//g')
+	tempCode=$(echo "$html" | grep "$1" -A1 | cut -d ">" -f 1 | sed -n 2p | xargs | sed 's/[+,]//g')
+	#comma needs to be removed to ensure the figure is an integer
 }
 
 clean_code '<span class="font-bold text-xl lg:text-2xl" data-v-1e2a93af data-v-91d5f596>'
-a=$tempCode
-echo "$a"
+dailyVaccine=$tempCode
+echo "$dailyVaccine"
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Cases</span>'
-
+dailyCases=$tempCode
+echo "$dailyCases"
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Daily - Tests</span>'
-
+dailyTests=$tempCode
+echo "$dailyTests"
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Positivity Rate</span>'
-
-e=$(echo "$html" | grep "Deaths due to COVID" -A2 | tail -n 1 | xargs)
-echo "$e" | sed 's/[+]//g'
-
+positiveRate=$tempCode
+echo "$positiveRate"
+dailyDeath=$(echo "$html" | grep "Deaths due to COVID" -A2 | tail -n 1 | xargs)
+echo "$dailyDeath" | sed 's/[+]//g'
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Active - ICU</span>'
-
+activeICU=$tempCode
+echo "$activeICU"
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Total - Cases</span>'
+totalCase=$tempCode
+echo "$totalCase"

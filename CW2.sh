5d29fd4e528fa354381f63f5a271f00d28adb2d9
Lim Han Jet <alexlim193@gmail.com>
2022-05-02 17:41:12 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -114,7 +114,7 @@ table4="death"
 		PRIMARY KEY (death_id)\
 	);\
 	
-	INSERT INTO $table1 (case_id, date, new_cases, total_cases, daily_tests, positivity_rate, active_cases, updated_time)
+	INSERT INTO $table1 (case_id, date, new_cases, total_cases, daily_tests, positivity_rate, active_cases, updated_time)\
 	VALUES (001, '$date', $dailyCases, $totalCase, $dailyTests, '$positiveRate', $activeCases, NOW());\
 	
 	SELECT * FROM $table1;\

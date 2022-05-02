696ad94da3c97d473476d453fc36599045c40fac
Lim Han Jet <alexlim193@gmail.com>
2022-05-02 18:58:06 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -19,7 +19,7 @@ dailyTests=$tempCode
 echo "$dailyTests"
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Positivity Rate</span>' 1
 positiveRate=$tempCode
-echo "$positiveRate" | sed 's/%//g'
+echo "Positive: $positiveRate" | sed 's/%//g'
 dailyDeath=$(echo "$html" | grep "Deaths due to COVID" -A2 | tail -n 1 | xargs)
 echo "$dailyDeath" | sed 's/[+]//g'
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Active - ICU</span>' 1
@@ -115,7 +115,7 @@ table4="death"
 	);\
 	
 	INSERT INTO $table1 (case_id, date, new_cases, total_cases, daily_tests, positivity_rate, active_cases, updated_time)\
-	VALUES (003, '$date', $dailyCases, $totalCase, $dailyTests, '$positive_Rate', $activeCases, NOW());\
+	VALUES (003, '$date', $dailyCases, $totalCase, $dailyTests, 'Positive', $activeCases, NOW());\
 	
 	SELECT * FROM $table1;\
 	SELECT * FROM $table2;\

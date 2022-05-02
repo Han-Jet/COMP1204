948f9401a1fac79522c020432fc16e185b6b804d
Lim Han Jet <alexlim193@gmail.com>
2022-05-02 21:30:26 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -19,7 +19,7 @@ dailyTests=$tempCode
 echo "$dailyTests"
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Positivity Rate</span>' 1
 positiveRate=$tempCode
-echo "Positive: $positiveRate" | sed 's/%//g'
+echo "$positiveRate" | sed 's/%//g'
 dailyDeath=$(echo "$html" | grep "Deaths due to COVID" -A2 | tail -n 1 | xargs)
 echo "$dailyDeath" | sed 's/[+]//g'
 clean_code '<span data-v-1e2a93af data-v-3ab42af2>Active - ICU</span>' 1
@@ -62,7 +62,7 @@ table1="cases"
 table2="vaccination"
 table3="healthcare"
 table4="death"
-rate="6.7"
+rate="$positiveRate"
 
 /opt/lampp/bin/mysql -u root -e "\
 	CREATE DATABASE IF NOT EXISTS $db;\

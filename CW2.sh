4912b970a389ea26b8ae5de42e67bb185f119a35
Lim Han Jet <alexlim193@gmail.com>
2022-05-02 23:05:55 +0800

    Update CW2.sh

--- CW2.sh
+++ CW2.sh
@@ -114,8 +114,7 @@ table4="death"
 		PRIMARY KEY (death_id)\
 	);\
 	
-	INSERT INTO $table1 (date, new_cases, total_cases, daily_tests, positivity_rate, active_cases, updated_time)
-	VALUES ('$date', $dailyCases, $totalCase, $dailyTests, '$positiveRate', $activeCases, NOW());\
+
 	INSERT INTO $table2 (date, daily_administered, total_administered, first_dose, two_doses, booster, updated_time)
 	VALUES ('$date', $dailyVaccine, $totalVaccine, '$firstDose', '$secondDose', '$booster', NOW());\
 	SELECT * FROM $table1;\

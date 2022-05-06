e962be568eb9fb0aa19dae0c215f39621d424ae6
Lim Han Jet <alexlim193@gmail.com>
2022-05-06 13:10:14 +0800

    Changed the size of the output
    1920*1080  -> 2160*1080

--- plot.sh
+++ plot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 shm="/dev/shm/"
 cases_file="${shm}case.txt"
 death_file="${shm}death.txt"
@@ -18,7 +17,7 @@ set xlabel "Date"
 set ylabel "Cases"
 set xtics 24*60*60
 set xdata time; set timefmt '%d %B %Y'; set format x '%d/%m'
-set terminal png size 1920,1080
+set terminal png size 2160,1080
 set output "$HOME/cases.png"
 plot "$cases_file" using 2:5 with lines notitle
 
